e03597013358a18a7415be5a2645ae0acfbcf0c3
Vyom <vyomj.home@gmail.com>
2025-02-06 21:42:50 -0800

    adding files and basic input(v0.02)

---
+++ guess_the_flag.js
@@ -0,0 +1,5 @@
+answer = document.getElementById("flagInput");
+document.addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById("mainImage").src = "nigeria.png";
+});
